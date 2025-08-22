b3afba453fca9eb84defec3aa01f5fd0e327fae4
oladimeji <oladimejisaeed@gmail.com>
2025-08-22 15:17:15 +0100

    feat(contact):add state handling for conatct form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,21 +1,49 @@
+import { useState } from "react";
+
 function Contact() {
+
+  const [FormData, setFormData] = useState ({
+    name : "",
+    email : "",
+    message : "", 
+  })
+
+  const handleChange = (e) => {
+    setFormData ({
+      ...FormData, [e.target.name] : e.target.value})
+  }
+
+  const handleSubmit =(e) => {
+    e.preventDefault();
+    console.log ("Form Submitted Successfully:", FormData)
+  }
   return (
     <div className="min-h-screen w-full max-w-[540px] shadow-md mx-auto bg-gray-100 flex flex-col p-4">
       <h1 className="text-2xl font-bold mb-4 text-blue-600">Contact Us</h1>
       <p className="text-gray-700">If you have any questions or feedback, feel free to reach out!</p>
-      <form className="space-y-4 mt-4">
+      
+      <form onSubmit={handleSubmit} className="space-y-4 mt-4">
         <input
           type="text"
+          name="name"
+          value={FormData.name}
+          onChange={handleChange}
           placeholder="Your name"
           className="w-full border p-2 rounded-md"
         />
         <input
           type="text"
+          name="email"
+          value={FormData.email}
+          onChange={handleChange}
           placeholder="Your Email"
           className="w-full border p-2 rounded-md"
         />
         <textarea
           placeholder="Your Message"
+          name="message"
+          value={FormData.message}
+          onChange={handleChange}
           rows="4"
           className="w-full border p-2 rounded-md"
         ></textarea>
